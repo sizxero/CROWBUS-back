1d5b28142351bb5d5831c8ed027f8021a7685fda
박유경 <pyk0514@kumoh.ac.kr>
2022-09-28 11:25:04 +0900

    Initial commit

---
+++ src/main/java/com/sizxero/crowbus/CrowbusApplication.java
@@ -0,0 +1,13 @@
+package com.sizxero.crowbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowbusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sizxero/crowbus/CrowbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sizxero.crowbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
